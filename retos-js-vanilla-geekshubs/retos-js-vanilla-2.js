abc5de2c5556983fa5b3d328020a3e529229b7fd
Paco Fuentes <pacofuentes.work@gmail.com>
2023-10-18 09:34:08 +0200

    retos-geeks-2-7

--- retos-js-vanilla-geekshubs/retos-js-vanilla-2.js
+++ retos-js-vanilla-geekshubs/retos-js-vanilla-2.js
@@ -379,3 +379,30 @@
 // • 1.28611 $ es un 1 €
 // • 129.852 yenes es un 1 €
  
+
+// const testPesetasPrompt = 1000;
+// // const euroPesetas = 166;
+// // console.log(testPesetasPrompt/euroPesetas);
+
+// const conversionDivisas = (euros, moneda) => {
+//     const euroPesetas = 166;
+//     switch (moneda) {
+//         case "pesetas":
+//             return `${euros} son ${euros*euroPesetas} pesetas`;
+//             break;
+    
+//         default:
+//             break;
+//     }
+// } 
+// console.log(conversionDivisas(6, "pesetas"));
+
+
+
+//  Reto-8 ---------------------------------------------------------------------
+
+// 8. Crea un array de 10 posiciones de números con valores pedidos por prompt. Muestra por consola el índice y el valor al que corresponde. Haz dos métodos, uno para
+// rellenar valores y otro para mostrar.
+
+
+// introduceValores
