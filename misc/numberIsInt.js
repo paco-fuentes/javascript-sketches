88cb07aec7ee02569b16c26047def35a8cf029a0
Paco Fuentes <pacofuentes.work@gmail.com>
2024-06-03 22:20:46 +0200

    update(codewars & misc): is square and number is integer

---
+++ misc/numberIsInt.js
@@ -0,0 +1,13 @@
+function fits(x, y) {
+    if (Number.isInteger(y / x)) {
+      return 'Fits!';
+    }
+    return 'Does NOT fit!';
+  }
+  
+  console.log(fits(5, 10));
+  // Expected output: "Fits!"
+  
+  console.log(fits(5, 11));
+  // Expected output: "Does NOT fit!"
+  
