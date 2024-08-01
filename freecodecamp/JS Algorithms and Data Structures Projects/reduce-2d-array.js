de9c5b20dd3180041f6f893004d066e607c4ab4e
Paco Fuentes <pacofuentes.work@gmail.com>
2024-08-01 21:14:40 +0200

    update(freecodecamp): update 2d array sum with reduce)

--- freecodecamp/JS Algorithms and Data Structures Projects/reduce-2d-array.js
+++ freecodecamp/JS Algorithms and Data Structures Projects/reduce-2d-array.js
@@ -1,13 +1,53 @@
+// const cid = [
+//   ["ONE", 1],
+//   ["ONE", 1],
+//   ["ONE", 1],
+//   ["QUARTER", 0.25],
+//   ["QUARTER", 0.25],
+// ];
+
+// const sumCid = cid.reduce((acc, e) => {
+//   return acc[e[0]] ? ++acc[e[0]] : (acc[e[0]] = 1), acc;
+// }, {});
+// const sumCidArr = Object.entries(sumCid);
+// console.table(sumCidArr);
+
+
 const cid = [
   ["ONE", 1],
   ["ONE", 1],
   ["ONE", 1],
+  ["ONE", 1],
+  ["ONE", 1],
+  ["ONE", 1],
+  ["ONE", 1],
+  ["ONE", 1],
+  ["ONE", 1],
+  ["ONE", 1],
+  ["ONE", 1],
+  ["ONE", 1],
   ["QUARTER", 0.25],
   ["QUARTER", 0.25],
+  ["QUARTER", 0.25],
+  ["QUARTER", 0.25],
+  ["QUARTER", 0.25],
+  ["DIME", 0.10],
+  ["DIME", 0.10],
+  ["DIME", 0.10],
 ];
 
-const sumCid = cid.reduce((acc, e) => {
-  return acc[e[0]] ? ++acc[e[0]] : (acc[e[0]] = 1), acc;
-}, {});
-const sumCidArr = Object.entries(sumCid);
-console.table(sumCidArr);
+const sumValuesByKey = (array) => {
+  const result = array.reduce((acc, [key, value]) => {
+    if (acc[key]) {
+      acc[key] += value;
+    } else {
+      acc[key] = value;
+    }
+    return acc;
+  }, {});
+
+  return Object.entries(result);
+};
+
+// Llamamos a la función y mostramos el resultado
+console.table(sumValuesByKey(cid)); // [["ONE", 3], ["QUARTER", 0.5]]
