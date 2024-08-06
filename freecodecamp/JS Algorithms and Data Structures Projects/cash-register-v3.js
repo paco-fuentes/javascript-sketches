1322d039325e84f3d2e188e881ffa1bd2aef8b14
Paco Fuentes <pacofuentes.work@gmail.com>
2024-08-06 21:03:26 +0200

    update(freecodecamp): cash register v3

--- freecodecamp/JS Algorithms and Data Structures Projects/cash-register-v3.js
+++ freecodecamp/JS Algorithms and Data Structures Projects/cash-register-v3.js
@@ -78,21 +78,7 @@ console.table(
     ["TWENTY", 60],
     ["ONE HUNDRED", 100],
   ])
-); // should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
-
-// console.table(
-//   checkCashRegister(3.26, 100, [
-//     ["PENNY", 1.01],
-//     ["NICKEL", 2.05],
-//     ["DIME", 3.1],
-//     ["QUARTER", 4.25],
-//     ["ONE", 90],
-//     ["FIVE", 55],
-//     ["TEN", 20],
-//     ["TWENTY", 60],
-//     ["ONE HUNDRED", 100],
-//   ])
-// ); // should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+); // should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
 
 // console.table(
 //   checkCashRegister(19.5, 20, [
