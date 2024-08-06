44d21229cf1a800099d9816f7a6369c09bd46089
Paco Fuentes <pacofuentes.work@gmail.com>
2024-08-06 20:36:35 +0200

    update(freecodecamp): update cash resgister versions and work in progress

--- freecodecamp/JS Algorithms and Data Structures Projects/cash-register-rafa.js
+++ freecodecamp/JS Algorithms and Data Structures Projects/cash-register-rafa.js
@@ -1,93 +1,108 @@
 const CURRENCY_UNITS = {
-    PENNY: 0.01,
-    NICKEL: 0.05,
-    DIME: 0.1,
-    QUARTER: 0.25,
-    ONE: 1,
-    FIVE: 5,
-    TEN: 10,
-    TWENTY: 20,
-    "ONE HUNDRED": 100,
-  };
-  
-  function checkCashRegister(price, cash, cid) {
-    const changeAmount = cash - price;
-    const totalCid = cid.reduce((acc, e) => acc + e[1] * 100, 0) * 0.01;
-  
-    if (changeAmount > totalCid) {
-      return { status: "INSUFFICIENT_FUNDS", change: [] };
-    }
-  
-    if (changeAmount === totalCid) {
-      return { status: "CLOSED", change: cid };
-    }
-  
-    const avaibleCurrencyUnits = (ch) =>
-      Object.values(CURRENCY_UNITS).filter((e, i) => {
-        return e <= ch && e <= cid[i][1];
-      });
-  
-    const currentCurrencyUnit = (ch) =>
-      avaibleCurrencyUnits(ch)[avaibleCurrencyUnits(ch).length - 1];
-  
-    const findCurrencyValue = (ch) =>
-      Object.entries(CURRENCY_UNITS)[
-        Object.values(CURRENCY_UNITS).indexOf(
-          Object.values(CURRENCY_UNITS).find((e) => e === currentCurrencyUnit(ch))
-        )
-      ] || "";
-  
-    //   console.log(changeAmount, totalCid);
-    if (changeAmount < totalCid) {
-      return cid
-        .reduce(
-          (acc, e, i, a) => {
-            const { currentChange } = acc[acc.length - 1];
-            const currentCurrency = findCurrencyValue(currentChange);
-            // console.log(currentChange, currentCurrency, currentChange - currentCurrency[1]);
-            return currentChange >= 0 && currentCurrency[1] !== undefined
-              ? [
-                  ...acc,
-                  {
-                    currentChange: currentChange - currentCurrency[1],
-                    change: currentCurrency,
-                  },
-                ]
-              : acc;
-          },
-          [{ currentChange: changeAmount, change: [] }]
-        )
-        .slice(1);
-    }
-  }
-  
-  console.table(
-    checkCashRegister(16.5, 20, [
-      ["PENNY", 1.01],
-      ["NICKEL", 2.05],
-      ["DIME", 3.1],
-      ["QUARTER", 4.25],
-      ["ONE", 90],
-      ["FIVE", 55],
-      ["TEN", 20],
-      ["TWENTY", 60],
-      ["ONE HUNDRED", 100],
-    ])
+  PENNY: 0.01,
+  NICKEL: 0.05,
+  DIME: 0.1,
+  QUARTER: 0.25,
+  ONE: 1,
+  FIVE: 5,
+  TEN: 10,
+  TWENTY: 20,
+  "ONE HUNDRED": 100,
+};
+
+const allCurrenciesLowerThan = (change) =>
+  Object.entries(CURRENCY_UNITS).filter(([currency, value]) => value <= change);
+
+const higherCurrencyLowerThan = (change) => (excludedCurrency) => {
+  const higherCurrency = allCurrenciesLowerThan(change)
+    .filter(([currencyName, _]) => currencyName !== excludedCurrency)
+    .slice(-1)[0];
+
+  const higherCurrencyAmount =
+    Math.floor(change / higherCurrency[1]) * higherCurrency[1];
+
+  return [higherCurrency[0], higherCurrencyAmount];
+};
+
+function checkCashRegister(price, cash, cid) {
+  const changeAmount = Number((cash - price).toFixed(2));
+  const totalCid = Number(
+    (cid.reduce((acc, e) => acc + e[1] * 100, 0) * 0.01).toFixed(2)
   );
-  
-  //
-  //   const bb = chCid.reduce((acc, e, i, a) => {
-  //     const {currentChange, change} = acc
-  //     const currentCurrency = findCurrencyValue(currentChange);
-  //     // console.log(currentChange, currentCurrency, currentChange - currentCurrency[1]);
-  //     return currentChange >= 0 && currentCurrency[1] !== undefined
-  //       ? {
-  //             currentChange: currentChange - currentCurrency[1],
-  //             change: [...change, currentCurrency]
-  //         }
-  //       : acc;
-  //   }, {currentChange: chAmt, change: []})
-  
-  // console.log(aa);
-  // return aa;
-  
+
+  if (changeAmount > totalCid) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  }
+
+  if (changeAmount === totalCid) {
+    return { status: "CLOSED", change: cid };
+  }
+
+  const processChange = cid
+    .reduce(
+      (acc, _, __, array) => {
+        const { currentChange, change } = acc[acc.length - 1];
+        if (currentChange <= 0) return acc;
+
+        const currentCurrency = higherCurrencyLowerThan(currentChange)(
+          change[0]
+        );
+
+        const currentCurrencyVal = Math.min(
+          array.find((val) => val[0] === currentCurrency[0])[1],
+          currentCurrency[1]
+        );
+
+        return [
+          ...acc,
+          {
+            currentChange: Number(
+              (currentChange - currentCurrencyVal).toFixed(2)
+            ),
+            change: [currentCurrency[0], currentCurrencyVal],
+          },
+        ];
+      },
+      [{ currentChange: changeAmount, change: [] }]
+    )
+    .slice(1)
+    .map((currencies) => currencies.change);
+
+  return changeAmount ===
+    Number(processChange.reduce((acc, value) => acc + value[1], 0).toFixed(2))
+    ? {
+        status: "OPEN",
+        change: processChange,
+      }
+    : { status: "INSUFFICIENT_FUNDS", change: [] };
+}
+
+console.table(
+  checkCashRegister(3.26, 100, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
+
+//
+//   const bb = chCid.reduce((acc, e, i, a) => {
+//     const {currentChange, change} = acc
+//     const currentCurrency = findCurrencyValue(currentChange);
+//     // console.log(currentChange, currentCurrency, currentChange - currentCurrency[1]);
+//     return currentChange >= 0 && currentCurrency[1] !== undefined
+//       ? {
+//             currentChange: currentChange - currentCurrency[1],
+//             change: [...change, currentCurrency]
+//         }
+//       : acc;
+//   }, {currentChange: chAmt, change: []})
+
+// console.log(aa);
+// return aa;
--- freecodecamp/JS Algorithms and Data Structures Projects/cash-register-refactor.js
+++ freecodecamp/JS Algorithms and Data Structures Projects/cash-register-refactor.js
@@ -11,35 +11,36 @@ const CURRENCY_UNITS = {
 };
 
 function checkCashRegister(price, cash, cid) {
-  const changeAmount = cash - price;
-  const initChangeAmount = Number(changeAmount.toFixed(2));
+  const initChangeAmount = Number((cash - price).toFixed(2));
   const totalCid = Number(cid.reduce((acc, e) => acc + e[1], 0).toFixed(2));
-  // el cambio es mayor al disponible
+  // caso el cambio es mayor al disponible
   if (initChangeAmount > totalCid) {
     return { status: "INSUFFICIENT_FUNDS", change: [] };
   }
-
   // caso el cambio es igual al total
   if (initChangeAmount === totalCid) {
     return { status: "CLOSED", change: cid };
   }
 
   // determinar el primer tipo de moneda a devolver:
-  // 1 - cantidad maxima disponible para el cambio ---> [0.1...20]
+  // 1 - cantidad máxima disponible para el cambio ---> [0.1...20]
+  // próximo tipo de currency que podría coger --->
   const avaibleCurrencyUnits = (ch, c) =>
     Object.entries(CURRENCY_UNITS)
-      .reverse()
+      .toReversed()
       .filter(([unit, value]) => {
         const cidAmount = c.find(([currUnit]) => currUnit === unit)[1] || 0;
         return value <= ch && value <= cidAmount;
       });
-  // moneda actual --> ['TEN', 10]
+
+  // 2 - último valor de la cantidad disponible con su currency ---> ['TWENTY', 20]
+  // próximo valor disponible en el cid --->
   const currentCurrencyUnit = (ch, c) =>
     avaibleCurrencyUnits(ch, c)[0] || ["", 0];
 
-  // caso el cambio es menor al total
+  // 3 - caso el cambio es menor al total
   // resto la 'current change' al 'amount'
-  // !!! ---> restar al elemento cid actual el valor de 'current currency' ---> cid[idx][1] - currentCurrency[1]
+  // restar a cada elemento del cid correspondiente el valor de 'current currency'
   if (initChangeAmount < totalCid) {
     const processChange = cid
       .reduce(
@@ -52,9 +53,11 @@ function checkCashRegister(price, cash, cid) {
 
           if (currencyValue === 0) return acc;
 
+          // cantidad de la moneda actual
           const cidAmount =
-            currentCid.find(([unit]) => unit === currencyName)?.[1] || 0;
-          const useAmount = Math.min(
+            currentCid.find(([val]) => val === currencyName)[1] || 0;
+
+          const currentCurrencyVal = Math.min(
             currencyValue * Math.floor(currentChange / currencyValue),
             cidAmount
           );
@@ -63,13 +66,15 @@ function checkCashRegister(price, cash, cid) {
             ? [
                 ...acc,
                 {
-                  currentChange: Number((currentChange - useAmount).toFixed(2)),
-                  change: [currencyName, useAmount],
-                  currentCid: currentCid.map(([unit, amount]) =>
-                    unit === currencyName
-                      ? [unit, Number((amount - useAmount).toFixed(2))]
-                      : [unit, amount]
+                  currentChange: Number(
+                    (currentChange - currentCurrencyVal).toFixed(2)
                   ),
+                  change: [currencyName, currentCurrencyVal],
+                  currentCid: currentCid.map(([unit, amount]) => {
+                    return unit === currencyName
+                      ? [unit, Number((amount - currentCurrencyVal).toFixed(2))]
+                      : [unit, amount];
+                  }),
                 },
               ]
             : acc;
@@ -82,14 +87,6 @@ function checkCashRegister(price, cash, cid) {
         acc[key] ? (acc[key] += value) : (acc[key] = value);
         return acc;
       }, {});
-    console.log(
-      initChangeAmount,
-      Number(
-        Object.values(processChange)
-          .reduce((a, b) => a + b, 0)
-          .toFixed(2)
-      )
-    );
 
     return initChangeAmount ===
       Number(
@@ -97,13 +94,11 @@ function checkCashRegister(price, cash, cid) {
           .reduce((a, b) => a + b, 0)
           .toFixed(2)
       )
-      ? { status: "OPEN", change: Object.entries(processChange) }
-      : { status: "INSUFFICIENT_FUNDS", change: [] };
+      ? // caso tengo cambio disponible
+        { status: "OPEN", change: Object.entries(processChange) }
+      : // caso tengo cantidad pero no tengo moneda de cambio
+        { status: "INSUFFICIENT_FUNDS", change: [] };
   }
-
-  //   if (initChangeAmount < avaibleCurrencyUnits) {
-  //     return { status: "INSUFFICIENT_FUNDS", change: [] };
-  //   }
 }
 
 console.table(
@@ -121,7 +116,7 @@ console.table(
 );
 
 console.table(
-  checkCashRegister(13.6, 20, [
+  checkCashRegister(183.28, 1183.47, [
     ["PENNY", 1.01],
     ["NICKEL", 2.05],
     ["DIME", 3.1],
@@ -129,7 +124,7 @@ console.table(
     ["ONE", 90],
     ["FIVE", 55],
     ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100],
+    ["TWENTY", 6000],
+    ["ONE HUNDRED", 850],
   ])
 );
